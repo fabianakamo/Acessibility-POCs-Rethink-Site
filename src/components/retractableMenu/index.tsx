5a49421c4201d0e353d8f2ff071527edfca7ce22
fabianakamo <fabiana.kamo@rethink.dev>
2023-01-26 18:23:47 -0300

    increase-decrease font size

--- src/components/retractableMenu/index.tsx
+++ src/components/retractableMenu/index.tsx
@@ -10,23 +10,55 @@ import light from "./lightTheme.module.css";
 import dark from "./darkTheme.module.css";
 
 const RetractableMenu = () => {
+  const [theme, setTheme] = useState<"light" | "dark">("dark");
+  const [fontNumber, setFontNumber] = useState(12);
+  // const [countFont, setCountFont] = useState(0);
   const [toggle, setToggle] = useState(false);
+
+  // Mudança de cor
   const newTheme = useTheme();
+  const currentTheme = useTheme();
+  const styles = { light, dark };
 
   const handleTheme = () => {
     const currentTheme = newTheme.theme.theme === "dark" ? "light" : "dark";
     newTheme.setCurrentTheme({ theme: currentTheme });
   };
 
-  const [theme, setTheme] = useState<"light" | "dark">("dark");
-
-  const currentTheme = useTheme();
-
   useEffect(() => {
     setTheme(currentTheme.theme.theme);
   }, [currentTheme]);
 
-  const styles = { light, dark };
+  // Mudança de letras
+  const increaseSize = () => {
+    if (fontNumber < 60 && fontNumber >= 10) {
+      setFontNumber((prevValue) => (prevValue += prevValue / 10));
+      document.documentElement.style.setProperty(
+        "--font-size",
+        `${fontNumber + 2}px`
+      );
+    }
+  };
+
+  const decreaseSize = () => {
+    if (fontNumber < 100 && fontNumber >= 10) {
+      setFontNumber((prevValue) => (prevValue -= prevValue / 10));
+      document.documentElement.style.setProperty(
+        "--font-size",
+        `${fontNumber - 2}px`
+      );
+    }
+  };
+
+  // const normalSize = () => {
+  //   if (fontNumber < 100 && fontNumber >= 10) {
+  //     setFontNumber((prevValue) => (prevValue -= prevValue / 10));
+  //     document.documentElement.style.setProperty(
+  //       "--font-size",
+  //       `${fontNumber - 2}px`
+  //     );
+  //   }
+  // };
 
   return (
     <div
@@ -50,18 +82,19 @@ const RetractableMenu = () => {
           id="options"
           className={`${mainTheme.content} ${toggle && mainTheme.active}`}
         >
-          {/* O TAB NAO PASSA POR AQUI */}
-          <div
+          {/* O TAB NAO PASSA POR AQUI - TEM QUE SER BUTTON*/}
+          {/* <div
             className={`${styles[theme].icon_increaseText} ${mainTheme.icon_increaseText}`}
           >
             <TextIncreaseIcon
               className={`${styles[theme].icon_increaseText} ${mainTheme.icon_increaseText}`}
             />
-          </div>
-
-          {/* <TextIncreaseIcon /> */}
+          </div> */}
+          {/* <button onClick={increaseSize}>A+</button> */}
+          <TextIncreaseIcon onClick={increaseSize} />
+          {/* <FormatClearIcon onClick={normalSize} /> */}
           <FormatClearIcon />
-          <TextDecreaseIcon />
+          <TextDecreaseIcon onClick={decreaseSize} />
           <ContrastIcon onClick={handleTheme} />
         </div>
       </div>
